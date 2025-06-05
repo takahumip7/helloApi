7022424125ccc402ff13a6ab022c3171571573ce
山下尊史 <yamashitatakahumi@MacBook-Air.local>
2025-06-06 08:01:43 +0900

    コミット

---
+++ src/main/java/com/example/api/Message.java
@@ -0,0 +1,20 @@
+package com.example.api;
+
+public class Message {
+	
+	private String message;
+	
+	public Message(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
